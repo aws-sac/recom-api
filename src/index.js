85e993d311176bf7fcb1192affad4b69c329722b
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2019-01-18 12:16:12 +0200

    Add recommandations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const profile = require('./data/profile.json');
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/recommandations', (req, res) => {
+  res.json(profile);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
